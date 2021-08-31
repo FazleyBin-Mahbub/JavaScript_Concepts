12cbc7821d6e1dd4e3e78008f0ad1cd51aae4634
Rabby-Cyber <ariyanrabby94@gmail.com>
2021-09-01 00:20:51 +0600

    callback

---
+++ js tricky concepts/callback.js
@@ -0,0 +1,13 @@
+function message(letter) {
+  console.log(letter);
+}
+
+const letter = "Go Fast, Never give up";
+
+message(letter);
+
+function greet() {
+  console.log("Good Morning");
+}
+
+message(greet);
