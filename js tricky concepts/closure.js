7ac8b48acbaf2a6ffe63cd06116b7f74a06c9962
Rabby-Cyber <ariyanrabby94@gmail.com>
2021-09-03 18:02:49 +0600

    closure

--- js tricky concepts/closure.js
+++ js tricky concepts/closure.js
@@ -7,6 +7,28 @@ function stopWatch() {
 }
 
 let clock1 = stopWatch();
-console.log(clock1());
+// console.log(clock1());
 
-console.log(clock1());
+// console.log(clock1());
+
+const bank = (owner) => {
+  balance = 0;
+  return {
+    deposit: (amount) => {
+      balance += amount;
+      return balance;
+    },
+    withdraw: (amount) => {
+      balance -= amount;
+      return balance;
+    },
+  };
+};
+
+const myBank = bank("Fazley");
+console.log(myBank.deposit(100));
+console.log(myBank.deposit(300));
+console.log(myBank.balance);
+console.log(myBank.deposit(300));
+console.log(myBank.withdraw(200));
+console.log(myBank.withdraw(100));
