8599c9e8488d7e5575ed8b6a526cdf0dd8270ab5
Rabby-Cyber <ariyanrabby94@gmail.com>
2021-09-04 18:54:25 +0600

    another scope

--- js tricky concepts/scope.js
+++ js tricky concepts/scope.js
@@ -16,3 +16,17 @@ const sum = add(11, 35);
 
 for (let i = 0; i < 10; i++) {}
 // console.log(i);
+
+function patroChai(age, earning) {
+  console.log(age);
+  console.log(earning);
+  let trueAge = age + 7;
+  function canMarry(expense) {
+    const remaining = earning - expense;
+    if (remaining > 1000) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
